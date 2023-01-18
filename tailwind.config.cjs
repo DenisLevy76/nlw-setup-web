75f703c74cd9c7dcd5ff4e602b0067fb9a983045
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-01-18 10:20:21 -0300

    Add: reset css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ['main-bg']: '#09090A',
+      },
+    },
   },
   plugins: [],
 }
