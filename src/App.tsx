6673f0029c2baf7aed7bba768995527a096b7ede
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-01-18 11:13:48 -0300

    Add: SummaryTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { HeaderComponent } from './components/HeaderComponent'
+import { SummaryTable } from './components/SummaryTable'
 
 export const App = () => {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl flex flex-col px-6 gap-16'>
         <HeaderComponent />
+        <SummaryTable />
       </div>
     </div>
   )
---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,11 @@
+export const SummaryTable: React.FC = () => {
+  return (
+    <main className='w-full flex'>
+      <div className='grid grid-rows-7 grid-flow-row gap-3'>
+        <div className='text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center uppercase'>
+          D
+        </div>
+      </div>
+    </main>
+  )
+}
